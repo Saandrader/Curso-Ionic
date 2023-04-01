a2fc4b960bef9f7ea542baa39d7f13476baf4918
Samuel Andrade <samuelandraderive@gmail.com>
2023-04-01 04:16:48 -0500

    Platzi 12/22 Fundamentos de Angular

--- src/app/pruebas/pruebas.page.ts
+++ src/app/pruebas/pruebas.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-pruebas',
@@ -7,19 +9,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PruebasPage implements OnInit {
 
-  constructor() { }
+  familia :any = [];
+
+
+
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.getUsers().subscribe(res=>{
+      console.log("Res",res)
+      this.familia = res;
+    });
   }
+
 
   name = 'Samuel';
+  inputText = 'Vanessa';
   img ='https://source.unsplash.com/random'
   btnDisabled = true;
-  friends = {
-    career : "ciencias politicas",
-    name : "gabriela"
+  ageNumber = 10;
+  newName ='';
 
+  person = {
+    name : "gabriela",
+    names: ['Daniel', 'Trosky', 'Oliver'],
+    age : 10
   }
 
+  toggleBtn(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  changeName(event : Event){
+    const element = event.target as HTMLInputElement
+    this.person.name = element.value;
+  }
+
+  increaseAge(){
+  this.person.age = Number(this.person.age) + 1;
+  }
+
+  onEnter(event:any){
+    if (event.keyCode == 13){
+      this.addName();
+    }
+  }
+
+
+
+  addName(){
+    this.person.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number){
+    this.person.names.splice(index,1);
+  }
+
+
+
+
+  getUsers(){
+    return this.http
+    .get("assets/files/customers.json")
+    .pipe(
+      map((res:any) =>{
+        return res.data;
+      })
+    )
+  }
 
 }
